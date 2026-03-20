9c18a48b2d04ea1e6e1fa8a2a466d63b8a893588
Kenny <kennedyelia00@icloud.com>
2026-03-19 23:43:29 -0700

    added hero animation

--- res.js
+++ res.js
@@ -4,3 +4,21 @@ const navMenu = document.getElementById("nav-menu");
 menuToggle.addEventListener('click',()=>{
     navMenu.classList.toggle("active");
 })
+
+
+const heroContent = document.querySelector(".hero-content");
+
+function animateHero() {
+    heroContent.classList.remove("animate");
+
+    // force reflow (important trick)
+    void heroContent.offsetWidth;
+
+    heroContent.classList.add("animate");
+}
+
+// run every 5 seconds
+setInterval(animateHero, 5000);
+
+// run once on load
+animateHero();
